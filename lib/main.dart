ee40cbec843aebcaa73faa7ace5372d95d0eb019
enpassio <abhishekraj@abhishekraj.me>
2019-07-24 23:59:35 +0530

    Created a stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 
-const _padding = EdgeInsets.all(16.0);
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.greenAccent,
+    );
+  }
+}
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Hello Rectangle',
-      theme: ThemeData(primarySwatch: Colors.green),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello Rectangle'),
+          title: Text("This is the title"),
         ),
         body: HelloRectangle(),
       ),
@@ -18,23 +23,47 @@ void main() {
   );
 }
 
-class HelloRectangle extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.greenAccent,
-        height: 400.0,
-        padding: _padding,
-        width: 300.0,
-        child: Center(
-          child: Text(
-            'Hello!, it\'s me a rectangle',
-            style: TextStyle(fontSize: 40.0),
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ),
+/*Rectangle sample */
+
+/**
+
+    const _padding = EdgeInsets.all(16.0);
+
+    void main() {
+    runApp(
+    MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Hello Rectangle',
+    theme: ThemeData(primarySwatch: Colors.green),
+    home: Scaffold(
+    appBar: AppBar(
+    title: Text('Hello Rectangle'),
+    ),
+    body: HelloRectangle(),
+    ),
+    ),
     );
-  }
-}
+    }
+
+    class HelloRectangle extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+    return Center(
+    child: Container(
+    color: Colors.greenAccent,
+    height: 400.0,
+    padding: _padding,
+    width: 300.0,
+    child: Center(
+    child: Text(
+    'Hello!, it\'s me a rectangle',
+    style: TextStyle(fontSize: 40.0),
+    textAlign: TextAlign.center,
+    ),
+    ),
+    ),
+    );
+    }
+    }
+
+ */
