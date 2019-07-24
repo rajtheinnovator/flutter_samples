6f1d20e3134129314d3267605a70762f776e14b9
enpassio <abhishekraj@abhishekraj.me>
2019-07-25 00:05:33 +0530

    Customized a widget by creating it from scratch

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 class HelloRectangle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.greenAccent,
+    return Center(
+      child: Container(
+        color: Colors.greenAccent,
+        height: 500,
+        width: 300,
+        child: Center(
+            child: Text(
+              "Hello",
+              style: TextStyle(fontSize: 40.0),
+              textAlign: TextAlign.center,
+            )),
+      ),
     );
   }
 }
@@ -22,48 +32,3 @@ void main() {
     ),
   );
 }
-
-/*Rectangle sample */
-
-/**
-
-    const _padding = EdgeInsets.all(16.0);
-
-    void main() {
-    runApp(
-    MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Hello Rectangle',
-    theme: ThemeData(primarySwatch: Colors.green),
-    home: Scaffold(
-    appBar: AppBar(
-    title: Text('Hello Rectangle'),
-    ),
-    body: HelloRectangle(),
-    ),
-    ),
-    );
-    }
-
-    class HelloRectangle extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-    return Center(
-    child: Container(
-    color: Colors.greenAccent,
-    height: 400.0,
-    padding: _padding,
-    width: 300.0,
-    child: Center(
-    child: Text(
-    'Hello!, it\'s me a rectangle',
-    style: TextStyle(fontSize: 40.0),
-    textAlign: TextAlign.center,
-    ),
-    ),
-    ),
-    );
-    }
-    }
-
- */
